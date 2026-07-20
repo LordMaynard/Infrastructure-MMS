343a2d163422a04661e4c317dfed18750826d77f
Maynard <onyekachivictor909@gmail.com>
2026-07-20 13:27:17 +0100

    DecisionMaking

--- DecisionMaking.py
+++ DecisionMaking.py
@@ -1,8 +1,12 @@
 road_condition = input("Enter Road Condition: ")
 
 if road_condition.lower() == "poor":
+    print("Maintenance Priority: HIGH")
     print("Immediate maintenance required.")
+
 elif road_condition.lower() == "fair":
+    print("Maintenance Priority: MEDIUM")
     print("Schedule maintenance soon.")
 else:
-    print("Road is in good condition.")
+    print("Maintenance Priority: LOW")
+    print("Continue routine inspection.")
