c9654060644246cfc1de3e907ba8f5e5dab45537
Maynard <onyekachivictor909@gmail.com>
2026-07-20 12:04:11 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,31 @@
+road_name = "East-West Road"
+road_id = 1001
+road_location = "Port Harcourt"
+road_length_km = 12.5
+road_width_m = 18
+road_condition = "Fair"
+
+last_inspection = "2026-07-15"
+last_repair = "2026-01-20"
+
+maintenance_cost = 4500000.50
+
+traffic_volume = 28000
+
+has_drainage = True
+
+print("ROAD INFORMATION")
+
+print("----------------------")
+
+print("Road Name:", road_name)
+print("Road ID:", road_id)
+print("Location:", road_location)
+print("Length:", road_length_km, "km")
+print("Width:", road_width_m, "meters")
+print("Condition:", road_condition)
+print("Last Inspection:", last_inspection)
+print("Last Repair:", last_repair)
+print("Maintenance Cost:", maintenance_cost)
+print("Traffic Volume:", traffic_volume)
+print("Drainage Available:", has_drainage)
